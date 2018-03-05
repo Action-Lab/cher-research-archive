99709622a3f028f4b9beb2caaed43d7307c90773
Ilya Ilyankou <ilya.ilyankou@trincoll.edu>
2018-03-04 21:11:26 -0500

    Fix CSS, add SelectAll-RemoveAll functionality

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
 var researchArchiveURL = 'https://docs.google.com/spreadsheets/d/1ianZrHebWkj2aKhOhkRbsk5p_4BvL__aImo6iuvADh4/pubhtml';
 
-// Sort by reverse chronology
-
-Tabletop.init({
-  key: researchArchiveURL,
-  reverse: true,
-  callback: processData,
-  simpleSheet: true,
-});
-
-function unifyDescription(t, a, r, p){
+function unifyDescription(t, a, r, p) {
   title = !!t ? "<h4>"+t+"</h4>" : '';
   abs = !!a ? "<p>Abstract:<br/>"+a+"</p>" : '<p>Unavailable</p>';
   pub = formatRepoURL(r);
@@ -17,13 +8,12 @@ function unifyDescription(t, a, r, p){
   return title + abs + pub + pos;
 }
 
-function formatPosterURL(url, showErr){
+function formatPosterURL(url, showErr) {
   if (!url) return !!showErr ? 'Unavailable' : '';
   return '<p><a target="_blank" href='+url+'>View poster</a> <a style="text-decoration: none;"href='+url+'><i class="fas fa-external-link-alt"></i></a></p>';
 }
 
-
-function formatRepoURL(url, showErr){
+function formatRepoURL(url, showErr) {
   if (!url) return !!showErr ? 'Unavailable' : '';
   Meta = ['<p>View Thesis:<br/>', '<p>View Theses:<br/>']
   Head = '<a target="_blank" href=';
@@ -42,61 +32,41 @@ function formatRepoURL(url, showErr){
 /*
  * Reformat year from say, 2018-2019, to 2018-19
  */
-function reformatYear(year){
+function reformatYear(year) {
   return /^\d{4}-\d{4}$/.test(year) ? year.slice(0,5)+year.slice(7,9) : year;
 }
 
-function processData(data, tabletop) {
-  if (!data[0]) return;
 
+var table;
+
+$(document).ready(function() {
   var processedData = [];
 
-  for (i in data) {
-    var r = data[i];
-    if (!!!r["Title "]) continue;
+  function processData(data, tabletop) {
+    if (!data[0]) return;
 
-    // Add a row to the final dataset
-    processedData.push([
-      reformatYear(r.Year),
-      r.Author,
-      r.Partner,
-      r.Subjects,
-      unifyDescription(r["Title "], r.Abstract, r.Repository, r.Poster),
-    ]);
-  }
+    for (i in data) {
+      var r = data[i];
+      if (!r['Title']) continue;
 
-  // Adding custom filtering
-  $.fn.dataTable.ext.search.push(
-    function(settings, data, dataIndex) {
-
-      // This is a JavaScript object whose keys will
-      // be the checked values (e.g. Arts, "Social Sciences")
-      showOnly = {};
-
-      $('input:checkbox:checked').each(function() {
-        showOnly[this.value] = 1;
-      });
-
-      var divisions = data[3].split(';').map(function(x) {return x.trim()});
-
-      for (i in divisions) {
-        if (showOnly[divisions[i]] === 1) {
-          return true;
-        }
-      }
-      return false;
+      // Add a row to the final dataset
+      processedData.push([
+        reformatYear(r.Year),
+        r.Author,
+        r.Partner,
+        r.Subjects,
+        unifyDescription(r['Title'], r.Abstract, r.Repository, r.Poster),
+      ]);
     }
-  );
 
-
-  $(document).ready(function() {
-    var table = $('#results').DataTable({
+    // Transform <table> element into fancy DataTables element
+    table = $('#results').DataTable({
       paging: false,
       info: false,
       ordering: true,
       data: processedData,
       columns: [
-        {title: 'Year', width:'45px'},
+        {title: 'Year', width: '45px'},
         {title: 'Author', width: '20px'},
         {title: 'Partner', width: '30px'},
         {title: 'Subjects', width: '20px'},
@@ -104,12 +74,7 @@ function processData(data, tabletop) {
       ],
       "order": [[0, "desc"]]
     });
-
-    $('#filtersCheckboxes>input').change(function() {
-      table.draw();
-    });
-
-  });
+  }
 
   // List of checkboxes to be added to the Topics list
   var filters = [
@@ -127,6 +92,7 @@ function processData(data, tabletop) {
     'Science',
     'Sustainability',
   ].map(function(x) {
+    // Creating checkboxes in DOM
     var name = x.split(' ')[0].toLowerCase();
     $('#filtersCheckboxes').append('<input type="checkbox" \
       id="' + name + '" name="' + name + '" value="' + x + '" checked>\
@@ -134,12 +100,60 @@ function processData(data, tabletop) {
   });
 
 
+  // Adding custom Subjects filtering
+  $.fn.dataTable.ext.search.push(
+    function(settings, data, dataIndex) {
+
+      // This is a JavaScript object whose keys will
+      // be the checked values (e.g. Arts, "Social Sciences")
+      showOnly = {};
+
+      $('input:checkbox:checked').each(function() {
+        showOnly[this.value] = 1;
+      });
+
+      var divisions = data[3].split(';').map(function(x) { return x.trim() });
+
+      for (i in divisions) {
+        if (showOnly[divisions[i]] === 1) {
+          return true;
+        }
+      }
+      return false;
+    }
+  );
+
+  // Initialize Tabletop
+  Tabletop.init({
+    'key': researchArchiveURL,
+    'callback': processData,
+    'simpleSheet': true,
+  });
+
+  // Open and close the dropdown menu with checkboxes on Subjects button click
   $('#filtersButton').click(function() {
     if ( $('#filtersCheckboxes').hasClass('invisible') ) {
-      $('#filtersCheckboxes').removeClass('invisible').addClass('visible');
+      $('#filtersCheckboxes, #filterHelpers').removeClass('invisible').addClass('visible');
     } else {
-      $('#filtersCheckboxes').removeClass('visible').addClass('invisible');
+      $('#filtersCheckboxes, #filterHelpers').removeClass('visible').addClass('invisible');
     }
   });
 
-}
+  // When user selects or deselects a checkbox, update the table
+  $('#filtersCheckboxes>input').change(function() {
+    table.draw();
+  });
+
+  // When removeAll is clicked, de-select all filter checkboxes
+  $('#removeAll').click(function() {
+    $('input:checkbox').prop('checked', false);
+    table.draw();
+  });
+
+  // When selectAll is clicked, select all filter checkboxes
+  $('#selectAll').click(function() {
+    $('input:checkbox').prop('checked', true);
+    table.draw();
+  });
+
+});
